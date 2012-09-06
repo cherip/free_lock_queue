6915b9a596c592c774b208df053e5b5ad7a43974
cherip_linux_64 <mkdirer@gmail.com>
2012-09-06 21:45:27 +0800

    .

--- main.cpp
+++ main.cpp
@@ -7,14 +7,14 @@
 #include "stdint.h"
 
 //const int count_per_thread_push = 100000000;
-const int count_per_thread_push = 100000000;
+const int count_per_thread_push = 1000000;
 
 const float CPU_MHZ = 2797; //use cat /proc/cpuinfo get the value
 const float CPU_tick_count_per_second = CPU_MHZ*1000*1000;
 
 const int CACAH_SIZE = 0xFFFFFF; 
-int num_thread_pop = 5;
-int num_thread_push = 1;
+int num_thread_pop = 10;
+int num_thread_push = 10;
 int real_queue_num = 5;
 
 #include <stddef.h>
@@ -47,7 +47,7 @@ struct pointer_t
 
 //typedef void * data_type;
 
-#define dummy_val NULL
+#define dummy_val 0
 
 typedef unsigned long long data_type;
 
@@ -113,6 +113,7 @@ public:
         pointer_t tail, next;
         node_t* nd = new node_t();
         nd->value = val;
+        //printf("%lld\n", val);
         while(true){
             tail = this->tail_; 
             next = tail.ptr->next;
@@ -144,7 +145,7 @@ public:
 
             if(head.ptr == tail.ptr){
                 if (next.ptr == NULL){ 
-                    return NULL;
+                    return 1;
                 }
                 pointer_t new_pt(next.ptr, tail.tag+1);
                 CAS2(&(this->tail_), tail, new_pt);
@@ -161,139 +162,31 @@ public:
     }
 };
 
-struct real_lock_free_queue {
-    void push(unsigned long long push_time) {
-        int cur = end;
-        int next = (cur + 1) & CACAH_SIZE;
-        if (next == head) {
-            printf("error\n");
-        }
-        cache[cur] = push_time;
-        end = next;
-        if (push_time != 0)
-            push_count++;
-    }
-
-    unsigned long long get_val() {
-        if (head == end) return 1;
-        if (cache[head] == 0) {
-        //    printf("get %d %d\n", head, end);
-        }
-        return cache[head];
-    }
-
-    bool pop() {
-        if (head == end) return true;
-        if (cache[head] == 0) {
-            printf ("pop %d\n", head);
-            return false;
-        }
-        //printf("pop %lld\n", cache[head]);
-        head = (head + 1) & CACAH_SIZE;
-        pop_count++;
-        return true;
-    }
-
-    real_lock_free_queue() {
-        head = 0;
-        end = 0;
-        push_count = 0;
-        pop_count = 0;
-        cache = new unsigned long long[CACAH_SIZE];
-    }
-
-    int head, end;
-    unsigned long long *cache;
-    int push_count;
-    int pop_count;
-};
 
 struct lock_free_queue   //ÎÒ°ÑÊµÏÖ²¿·ÖÈ¥µôÁË£¬¿òŒÜ¹©²Î¿Œ£¬ÆäÖÐŽòÓ¡²¿·Ö°ïÖúµ÷ÊÔ£¬È·¶šÑÓ³Ù¡£ÕýÊœ±ÈÆŽ¿ÉÒÔ×¢ÊÍµô¡£
 {
 	void push(unsigned long long pop_time)
 	{
-        int idx = get_index(push_count);
-        push_count++;
-        if (push_count % 10000000 == 0) {
-            printf("%.4f\n", (float)push_count / (count_per_thread_push * 10));
-            calc_total_push();
-        }
-
-        real_lock_free_queue *c_queue = &real_queue[idx];
-        //printf("push %d\n", idx);
-        for (;;) {
-            int cur = push_lock_flag[idx];
-            int new_val = 1 - cur;
-            if (cur == 0) {
-                if (__sync_bool_compare_and_swap(&push_lock_flag[idx], cur, new_val)) {
-                    c_queue->push(pop_time);
-                    push_lock_flag[idx] = 0;
-                    return;
-                }
-            }
-        }
+        real_queue->enqueue(pop_time);
 	}
 	bool pop()
 	{
-        for(;;) {
-            int quit = 1;
-            unsigned long long min_val = 0xFFFFFFFFFFFFFFF;
-            //printf("new round...\n");
-            for (int i = 0; i < real_queue_num; i++) {
-                unsigned long long tmp = real_queue[i].get_val(); 
-                if (tmp != 0 && tmp != 1) {
-                    if (pop_lock_flag[i] == 0) {
-                        if (__sync_bool_compare_and_swap(&pop_lock_flag[i], 0, 1)) {
-                            bool ret = real_queue[i].pop();
-                            pop_lock_flag[i] = 0;
-                            return true;
-                        }
-                    }
-                }
-                if (tmp != 0) quit = 0;
-            }
-            if(quit) return false;
-        }
+        if (real_queue->dequeue() == 0) 
+            return false;
+        return true;
 	};
-    real_lock_free_queue* get_queue(int k) {
-        return &real_queue[k];
-    }
-
-    int get_index(int k) {
-        //printf("idx: %d\n", (k * 3) % real_queue_num);
-        return (k * 3) % real_queue_num;
-    }
-
-    int calc_total_push() {
-        unsigned long long total = 0;
-        unsigned long long total_pop = 0;
-        for (int i = 0; i < real_queue_num; ++i) {
-            total += real_queue[i].push_count;
-            total_pop += real_queue[i].pop_count;
-        }
-        printf("%lld %lld %lld\n", total, total_pop, total - total_pop);
-    }
 
 	lock_free_queue()
 	{
-        push_count = 0;
-        real_queue = new real_lock_free_queue[real_queue_num];
-        push_lock_flag = new int[real_queue_num];
-        pop_lock_flag = new int[real_queue_num];
-        for (int i = 0; i < real_queue_num; i++) {
-            push_lock_flag[i] = 0;
-            pop_lock_flag[i] = 0;
-        }
+        real_queue = new queue_t;
+        real_queue->init();
 	};
 
 	~lock_free_queue()
 	{
 	}
-
-    volatile int push_count;
-    real_lock_free_queue *real_queue;
-    volatile int *push_lock_flag;
-    volatile int *pop_lock_flag;
+    
+    queue_t *real_queue;
 };
 
 void* pop(void* queue)
@@ -367,7 +260,6 @@ int main(void)
         }
 	
     printf("total push:\n");
-    lfq.calc_total_push();
     printf("costs %.3f\n", (float)(clock() - start) / CLOCKS_PER_SEC);
 
 	if( NULL != thread_pop )
